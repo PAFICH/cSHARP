0f9772a9f45cfe06523b2e38520cf2aa431b352e
Pavlin Valyovski <PAFICH@users.noreply.github.com>
2016-04-25 18:18:41 +0300

    Added files via upload

---
+++ 03.Operators-And-Statements/12.ExtractBitFromInteger/ExtractBitFromInteger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ExtractBitFromInteger
+{
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine());
+        int position = int.Parse(Console.ReadLine());
+
+        long foundBit = number >> position;
+        long bit = foundBit & 1;
+        
+        Console.WriteLine(bit);
+    }
+}
